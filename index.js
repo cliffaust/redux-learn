164684920f4a2c8aac9d0bac8c69acf44243e672
Clifford <kingsleyclifford2016@gmail.com>
2021-10-22 22:46:16 +0000

    initial commit

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function buyCake() {
     info: "first redux action",
   };
 }
+
+const initialState = {
+  numOfCake: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        numOfCake: state.numOfCake - 1,
+      };
+  }
+};
